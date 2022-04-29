30e442ab4f48d2222fd3aa7ed1500938275944a5
Renato Lui Geh <renatogeh@gmail.com>
2022-04-29 15:50:25 -0300

    Few adjustments

--- src/LaTeXUtils.jl
+++ src/LaTeXUtils.jl
@@ -14,6 +14,11 @@ mutable struct Table
     else data = M end
     return new(T, data, D)
   end
+
+  function Table(M::AbstractArray)::Table
+    n, m = size(M)
+    return Table(fill("", m), M, fill("", n))
+  end
 end
 export Table
 
@@ -46,24 +51,29 @@ end
 @inline Base.getindex(T::Table, I::Int...) = getindex(T.M, I...)
 @inline Base.setindex!(T::Table, v, K...) = setindex!(T.M, v, K...)
 
+function ranks(T::Table; kwargs...)::Matrix
+  n, m = size(T.M)
+  P = [sortperm(T.M[i,:]; kwargs...) for i ∈ 1:n]
+  R = Matrix{Int}(undef, n, m)
+  for i ∈ 1:n, j ∈ 1:m R[i,P[i][j]] = j end
+  return R
+end
+export ranks
+
 function Base.write(T::Table, path::String; highlight::Bool = true, kwargs...)
   f = open(path, "w")
   n, m = size(T.M)
-  write(f, "\\begin{table}\n\\begin{tabular}{" * repeat("l|", m) * "l}\n\\hline\n")
-  if highlight
-    P = [sortperm(T.M[i,:]; kwargs...) for i ∈ 1:n]
-    R = Matrix(undef, n, m)
-    for i ∈ 1:n, j ∈ 1:m R[i,P[i][j]] = j end
-  end
-  content = "\\textbf{Data} & "
+  write(f, "\\begin{table}\n\\begin{tabular}{" * repeat("l|", m-1) * "l}\n")
+  if highlight R = ranks(T; kwargs...) end
+  content = ""
   for i ∈ 1:m-1 content *= "\\textbf{\\textsc{$(T.T[i])}} & " end
   content *= "\\textbf{\\textsc{$(T.T[m])}}\\\\\n\\hline\n"
   for i ∈ 1:n
     content *= @sprintf("\\textsc{%s}", T.D[i])
     for j ∈ 1:m
-      if R[i,j] == 1 content *= @sprintf(" & \\textbf{%.2f}", T.M[i,j])
-      elseif R[i,j] == 2 content *= @sprintf(" & \\underline{%.2f}", T.M[i,j])
-      elseif R[i,j] == 3 content *= @sprintf(" & \$|\$%.2f\$|\$", T.M[i,j])
+      if highlight && R[i,j] == 1 content *= @sprintf(" & \\textbf{%.2f}", T.M[i,j])
+      elseif highlight && R[i,j] == 2 content *= @sprintf(" & \\underline{%.2f}", T.M[i,j])
+      elseif highlight && R[i,j] == 3 content *= @sprintf(" & \$|\$%.2f\$|\$", T.M[i,j])
       else content *= @sprintf(" & %.2f", T.M[i,j]) end
     end
     content *= "\\\\\n"
